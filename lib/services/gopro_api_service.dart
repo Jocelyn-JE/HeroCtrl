fed8e3f24dd233ecf1b348d2bb11a8d502df21ec
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-10-23 11:35:38 +0200

    ♻️ Refactor GoProApiService to implement singleton pattern and enhance command handling

--- lib/services/gopro_api_service.dart
+++ lib/services/gopro_api_service.dart
@@ -4,10 +4,30 @@ import 'package:heroctrl/models/camera_status.dart';
 import 'package:heroctrl/core/constants/gopro_endpoints.dart';
 
 class GoProApiService {
-  GoProApiService();
+  static final GoProApiService _instance = GoProApiService._internal();
+  static const _camera = 0;
+  static const _bacpac = 1;
+  static const devices = ['camera', 'bacpac'];
 
-  Future<CameraStatus> getStatus() async {
-    final response = await http.get(Uri.parse(GoProEndpoints.status));
+  factory GoProApiService() => _instance;
+
+  GoProApiService._internal();
+
+  Future<void> turnOffCamera(String password) async {
+    await _postApi(_bacpac, GoProEndpoints.power, password, Power.off);
+  }
+
+  Future<void> turnOnCamera(String password) async {
+    await _postApi(_bacpac, GoProEndpoints.power, password, Power.on);
+  }
+
+  Future<bool> cameraTurnedOn(String password) async {
+    final response = await _getApi(_bacpac, GoProEndpoints.power, password);
+    return response.bodyBytes[1] == 1;
+  }
+
+  Future<CameraStatus> getStatus(String password) async {
+    final response = await _getApi(_camera, GoProEndpoints.status, password);
     if (response.statusCode == 200) {
       final bytes = response.bodyBytes;
       AppLogger.info('GoPro Status: ${bytes.length} bytes');
@@ -18,15 +38,49 @@ class GoProApiService {
     }
   }
 
-  Future<void> _sendCommand(String path) async {
-    final response = await http.get(
-      Uri.parse('${GoProEndpoints.baseUrl}$path'),
-    );
-    if (response.statusCode != 200) {
+  Future<http.Response> _getApi(
+    int device,
+    String command,
+    String? password,
+  ) async {
+    if (device != _camera && device != _bacpac) {
+      throw Exception('Invalid device: $device');
+    }
+
+    String path =
+        '${GoProEndpoints.baseUrl}/${devices[device]}/${command.toLowerCase()}';
+    if (password != null) path += '?t=$password';
+    final response = await http.get(Uri.parse(path));
+    if (response.statusCode != 200 || response.bodyBytes[0] != 0) {
+      AppLogger.error(
+        'Failed to send command to GoPro: $path\n Response: ${response.bodyBytes.toString()}',
+      );
+      throw Exception('Command failed: $path');
+    }
+    return response;
+  }
+
+  Future<http.Response> _postApi(
+    int device,
+    String command,
+    String? password,
+    String? option,
+  ) async {
+    if (device != _camera && device != _bacpac) {
+      throw Exception('Invalid device: $device');
+    }
+
+    String path =
+        '${GoProEndpoints.baseUrl}/${devices[device]}/${command.toUpperCase()}';
+    if (password != null) path += '?t=$password';
+    if (option != null) path += '${password != null ? '&' : '?'}p=%$option';
+    final response = await http.get(Uri.parse(path));
+    if (response.statusCode != 200 || response.bodyBytes[0] != 0) {
       AppLogger.error(
         'Failed to send command to GoPro: $path\n Response: ${response.body}',
       );
       throw Exception('Command failed: $path');
     }
+    return response;
   }
 }
