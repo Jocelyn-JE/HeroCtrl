63563d067b3ab6b185b87ccb3fb3d3cb0f838e74
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-11-04 15:32:58 +0100

    ✨ Add Forget All Cameras functionality to SettingsScreen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:heroctrl/services/gopro_prefs.dart';
+import 'package:heroctrl/widgets/red_button.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final double bottomInset = MediaQuery.of(context).padding.bottom;
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: const Center(child: Text('Settings go here')),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(8, 0, 8, bottomInset),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      final navigatorState = Navigator.of(context);
+                      return AlertDialog(
+                        title: const Text('Forget all cameras'),
+                        content: const Text(
+                          'Are you sure you want to forget all cameras?',
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              navigatorState.pop();
+                            },
+                            child: const Text('Cancel'),
+                          ),
+                          RedButton(
+                            onPressed: () async {
+                              await GoProPrefs.clearAll();
+                              navigatorState.pop();
+                            },
+                            child: const Text('Forget'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Forget all cameras'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
