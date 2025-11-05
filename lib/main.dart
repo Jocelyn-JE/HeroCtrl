a6b8e64945ebab72a52b8c24d0071f8bf40835a6
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-11-05 11:47:47 +0100

    ♻️ Remove GoProApiService initialization from main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:heroctrl/core/utils/logger.dart';
-import 'package:heroctrl/services/gopro_api_service.dart';
 
 import 'package:heroctrl/screens/home_screen.dart';
 import 'package:heroctrl/screens/settings_screen.dart';
@@ -8,7 +7,6 @@ import 'package:heroctrl/screens/camera_search_screen.dart';
 
 void main() {
   AppLogger.init(); // Initialize logger
-  GoProApiService(); // Initialize singleton
   AppLogger.info('App started');
   runApp(const MainApp());
 }
