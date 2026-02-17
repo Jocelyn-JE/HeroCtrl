9311d531eba2e28a644969d080c40b89e38bf4f1
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2026-02-17 16:35:28 +0100

    💄 Improve showSnackBar function - Add text styling and add specific snackbar functions for error, success, and warning messages

--- lib/utils/snackbar.dart
+++ lib/utils/snackbar.dart
@@ -3,7 +3,22 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String message, {Color? color}) {
   if (!context.mounted) return;
   ScaffoldMessenger.of(context).clearSnackBars();
-  ScaffoldMessenger.of(
-    context,
-  ).showSnackBar(SnackBar(content: Text(message), backgroundColor: color));
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message, style: TextStyle(color: Colors.white)),
+      backgroundColor: color,
+    ),
+  );
+}
+
+void showSnackBarError(BuildContext context, String message) {
+  showSnackBar(context, message, color: Colors.red);
+}
+
+void showSnackBarSuccess(BuildContext context, String message) {
+  showSnackBar(context, message, color: Colors.green);
+}
+
+void showSnackBarWarning(BuildContext context, String message) {
+  showSnackBar(context, message, color: Colors.orange);
 }
